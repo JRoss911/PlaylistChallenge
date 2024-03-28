3a7f4a3906b0e9ca76c26688f08916d8345dcd52
Jorden Ross <JRoss9117@gmail.com>
2024-03-28 01:05:00 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,26 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    //have to compare starting index to index of selected song
+    //return the index distance between the two
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        Integer i = 0;
+        --i;
+        Integer selectionIndex = Arrays.asList(playList).indexOf(selection);
+        {
+            if (i < playList.length || i > playList.length) {
+                return selectionIndex - startIndex % 1 -1;
+            }
+        }
+        return i;
     }
 }
